fb48ac34ecdb791a6776fdb5cf5c879c32b6aaf9
jose <boxingamateur14@gmail.com>
2026-09-01 10:34:15 -0300

    feat(infra): Add TransferenciaService with ACID transactions and App entrypint

---
+++ src/BankTransfers.Infrastructure/Repositories/CuentaRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using BankTransfers.Domain.Entities;
+using BankTransfers.Infrastructure.Data;
+
+namespace BankTransfers.Infrastructure.Repositories;
+
+public class CuentaRepository
+{
+    private readonly DatabaseConnection _dbConnection;
+
+    public CuentaRepository()
+    {
+        _dbConnection = new DatabaseConnection();
+    }
+
+    public Cuenta? ObtenerPorId(int idCuenta)
+    {
+        using var connection = _dbConnection.GetConnection();
+        connection.Open();
+
+        var query = "SELECT IdCuenta, NumeroCuenta, IdCliente, Saldo, Estado, FechaCreacion FROM Cuentas WHERE IdCuenta = @IdCuenta";
+        using var command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@IdCuenta", idCuenta);
+
+        using var reader = command.ExecuteReader();
+        if (reader.Read())
+        {
+            return new Cuenta
+            {
+                IdCuenta = (int)reader["IdCuenta"],
+                NumeroCuenta = (string)reader["NumeroCuenta"],
+                IdCliente = (int)reader["IdCliente"],
+                Estado = (string)reader["Estado"],
+                FechaCreacion = (DateTime)reader["FechaCreacion"]
+            };
+        }
+
+        return null;
+    }
+
+    public void ActualizarSaldo(int idCuenta, decimal nuevoSaldo, SqlTransaction transaction, SqlConnection connection)
+    {
+        var query = "UPDATE Cuentas SET Saldo = @Saldo WHERE IdCuenta = @IdCuenta";
+        using var command = new SqlCommand(query, connection, transaction);
+        command.Parameters.AddWithValue("@Saldo", nuevoSaldo);
+        command.Parameters.AddWithValue("@IdCuenta", idCuenta);
+        command.ExecuteNonQuery();
+    }
+}
